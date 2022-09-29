b28a59f8f58b1864a6b4911bbf3d36acbe4801bd
Noelani West <noelani.rose14@gmail.com>
2022-09-29 10:37:18 -0500

    create random number generator

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
+console.log('in server.js');
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
+
+let tiaGuesses = []
+let laniguess = []
+
+
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -13,5 +19,22 @@ app.use(express.static('server/public'));
 
 
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
+  console.log ('We\'re LIVE!!! Server is running on port', PORT)
+  console.log(getRanNumber());
 })
+
+
+function getRanNumber (){
+  let ranNum = Math.floor(Math.random() * 25) + 1;
+  // if ($(this) === currentGuess){
+  //   alert('YOU ROCK!!!')
+  // }
+  // else if ($(this) > currentGuess){
+  //   alrert('you\'re sooooo high')
+  // }
+  // else ($(this) < currentGuess){
+  //   alert('too slow and too low')
+  // };
+  return ranNum;
+
+}
